6ed0ee99d95809cc4be034d8789e531fd397c881
Enzo Xavier <seu_email@exemplo.com>
2026-06-16 19:22:12 -0300

    feat(chess): ChessExpection

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -22,7 +23,11 @@ public class ChessMatch {
         return mat;
     }
 
+    public void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     public void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
